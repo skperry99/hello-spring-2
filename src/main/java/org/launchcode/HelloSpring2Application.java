3ccc9c356ddd161a7fd0031277fa0857bb59336c
Sarah Perry <sperry939@gmail.com>
2017-03-30 14:07:31 -0500

    Initial commit

---
+++ src/main/java/org/launchcode/HelloSpring2Application.java
@@ -0,0 +1,12 @@
+package org.launchcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpring2Application.class, args);
+	}
+}
---
+++ src/main/java/org/launchcode/controllers/HelloController.java
@@ -0,0 +1,24 @@
+package org.launchcode.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.xml.ws.RequestWrapper;
+
+/**
+ * Created by Maggie on 3/30/2017.
+ */
+@Controller
+public class HelloController {
+    @RequestMapping(value = "")
+    @ResponseBody
+    public String index() {
+        return "Hello World";
+    }
+    @RequestMapping(value = "goodbye")
+    @ResponseBody
+    public String goodbye() {
+        return "Goodbye";
+    }
+}
