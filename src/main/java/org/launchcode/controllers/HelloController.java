a4f1a2e7d4ac8a0f8672abbb0987d2422f379eb7
Sarah Perry <sperry939@gmail.com>
2017-03-30 14:46:53 -0500

    Added controllers to input and greet by name

--- src/main/java/org/launchcode/controllers/HelloController.java
+++ src/main/java/org/launchcode/controllers/HelloController.java
@@ -1,10 +1,11 @@
 package org.launchcode.controllers;
 
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.xml.ws.RequestWrapper;
 
 /**
  * Created by Maggie on 3/30/2017.
@@ -13,12 +14,46 @@ import javax.xml.ws.RequestWrapper;
 public class HelloController {
     @RequestMapping(value = "")
     @ResponseBody
-    public String index() {
-        return "Hello World";
+    public String index(HttpServletRequest request) {
+
+        String name = request.getParameter("name");
+
+        if(name == null){
+            name = "World";
+        }
+
+        return "Hello " + name;
     }
-    @RequestMapping(value = "goodbye")
+
+    @RequestMapping(value = "hello", method = RequestMethod.GET)
     @ResponseBody
+    public String hellowForm(){
+        String html = "<form method='post'>" +
+                "<input type='text' name='name' />" +
+                "<input type='submit' value='Greet Me!' />" +
+                "</form>";
+
+        return html;
+    }
+
+    @RequestMapping(value = "hello", method= RequestMethod.POST)
+    @ResponseBody
+    public String helloPost(HttpServletRequest request){
+
+        String name = request.getParameter("name");
+
+        return "Hello " + name;
+    }
+
+    @RequestMapping(value = "hello/{name}")
+    @ResponseBody
+    public String helloUrlSegment(@PathVariable String name) {
+
+        return "Hello " + name;
+    }
+
+    @RequestMapping(value = "goodbye")
     public String goodbye() {
-        return "Goodbye";
+        return "redirect:/";
     }
 }
